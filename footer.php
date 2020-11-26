c4dc8ce7c9de36698e61bc85803e5a181d6cc03d
Peter von Rohr <peter.vonrohr@qualitasag.ch>
2020-11-26 17:01:47 +0100

    Changed text in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 	<footer>
-	<p>Solange Duruz, <a href="http://people.epfl.ch/stephane.joost?lang=fr" target="_blank"><font color="#FFFFFF">St&#233;phane Joost</font></a>, Jens Ingensand. Inluding data and application from <a href="http://popreport.tzv.fal.de/cgi-bin/entry.pl">FLI</a> and <a href="http://www.wsl.ch/fe/landschaftsdynamik/projekte/FreeToDownload/index_EN">WSL</a><br>
-	<a href="http://lasig.epfl.ch" target="_blank" title="LASIG @ EPFL"><font color="#FFFFFF">Laboratory of Geographic Information Systems (LASIG)</font></a>,<br /> Ecole Polytechnique F&eacute;d&eacute;rale de Lausanne (EPFL), September 2013 LASIG - All rights reserved</p>						
+	<p>Qualitas AG - based on results from <a href="https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0176362" target="_blank">Duruz et al. (2017)</a>. Inluding data and application from <a href="http://popreport.tzv.fal.de/cgi-bin/entry.pl">FLI</a> and <a href="http://www.wsl.ch/fe/landschaftsdynamik/projekte/FreeToDownload/index_EN">WSL</a><br>
+	All rights reserved</p>						
 	</footer>
 
